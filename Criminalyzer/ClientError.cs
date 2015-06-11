bd4ef79eaba69aca23ecc21376a9211974c8df02
jamescadd <jamescadd@gmail.com>
2015-06-11 13:41:12 -0700

    Adding Oxford APIs

---
+++ Criminalyzer/ClientError.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criminalyzer
+{
+    public class ClientError
+    {
+        /// <summary>
+        /// Gets or sets error code in error entity.
+        /// </summary>
+        /// <value>
+        /// The code of client error.
+        /// </value>
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
+        public string Message
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the request identifier.
+        /// </summary>
+        /// <value>
+        /// The request identifier.
+        /// </value>
+        public Guid RequestId
+        {
+            get;
+            set;
+        }
+    }
+}
--- Criminalyzer/MainViewModel.cs
+++ Criminalyzer/MainViewModel.cs
@@ -6,6 +6,11 @@ using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight.Command;
 using PropertyChanged;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Criminalyzer
 {
@@ -14,7 +19,11 @@ namespace Criminalyzer
     {
         public RelayCommand GetMugshotsCommand {  get { return new RelayCommand(OnGetMugshots); } }
 
+        public RelayCommand TakePictureCommand { get { return new RelayCommand(OnTakePicture); } }
+
         public ObservableCollection<Record> Records = new ObservableCollection<Record>();
+
+        public BitmapImage CapturedImage { get; private set; }
 
         public MainViewModel()
         {
@@ -24,6 +33,59 @@ namespace Criminalyzer
         protected void OnGetMugshots()
         {
 
+        }
+
+        protected async void OnTakePicture()
+        {
+            //CameraCaptureUI dialog = new CameraCaptureUI();
+            //var photo = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            //if (photo == null)
+            //{
+            //    return;
+            //}
+
+
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".jpeg");
+            openPicker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                //OutputTextBlock.Text = "Picked photo: " + file.Name;
+                BitmapImage bitmapImage = new BitmapImage();
+                using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    bitmapImage.SetSource(fileStream);
+                }
+
+                CapturedImage = bitmapImage;
+            }
+            else
+            {
+                //OutputTextBlock.Text = "Operation cancelled.";
+            }
+
+
+
+            //var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            //var photo = await picker.PickSingleFileAsync();
+
+            //if (photo == null)
+            //    return;
+
+            //BitmapImage bitmapImage = new BitmapImage();
+            //using (IRandomAccessStream fileStream = await photo.OpenAsync(FileAccessMode.Read))
+            //{
+            //    bitmapImage.SetSource(fileStream);
+            //}
+
+            //CapturedImage = bitmapImage;
         }
     }
 }
